8f11569eb23ff30fc1bc0c2dc62144ce703ec0e6
youshamahmood <youshamahmood96@gmail.com>
2020-08-04 17:00:17 +0600

    Final Commit

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function pinGenerator(id1,id2){
         let result = Math.random() * (max - min) + min;
         let pin = Math.round(result);
         document.getElementById(id1).value = pin
+        document.getElementById('typed-pin').value = '';
     })
 }
 pinGenerator('pin-display','pin-generator-button')
